2ff8dbf35ff8500e7119b27dbc41784a0f2a678d
Karim <kbiscaiburo@gmail.com>
2024-08-12 12:02:12 -0300

    Operadores

---
+++ operadores.php
@@ -0,0 +1,23 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 20;
+
+// Sumar
+echo $numero1 + $numero2;
+echo "<br>";
+
+// Restar
+echo $numero1 - $numero2;
+echo "<br>";
+
+// Multiplicar
+echo $numero1 * $numero2;
+echo "<br>";
+
+// Dividir
+echo $numero1 / $numero2;
+echo "<br>";
+
+// Potencia
+echo 5 ** 5;
