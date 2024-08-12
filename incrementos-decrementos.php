05031b71253e568683f958e287ecb2b6ea713ae0
Karim <kbiscaiburo@gmail.com>
2024-08-12 12:22:47 -0300

    Incrementos y Decrementos

---
+++ incrementos-decrementos.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero1 = 10;
+
+echo $numero1++; // 10
+echo $numero1; // 11
+echo ++$numero1; // 12
+
+$numero2 = 20;
+echo $numero2--; // 20 
+echo $numero2; // 19
+echo --$numero2; // 18
