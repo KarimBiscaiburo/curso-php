d68ccf863060b0d89c6a815b2c603e42f3484412
Karim <kbiscaiburo@gmail.com>
2024-08-12 11:54:29 -0300

    Tipos de variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+
+// Sigue los mismos estandares para declarar variables que JS
+$nombre = "Juan";
+
+$edad = 2;
+echo $edad;
+
+$edad = 3;
+echo $edad;
+
+// Constantes
+define("constante", "Este es el valor de la constante");
+echo constante;
+
+// No se usa mucho
+const otraConstante = "Otra manera que no se usa tanto";
