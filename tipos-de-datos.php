89248d4a455b7224fd67b477dd3f5456d47b697f
Karim <kbiscaiburo@gmail.com>
2024-08-12 11:55:06 -0300

    Tipos de datos

---
+++ tipos-de-datos.php
@@ -0,0 +1,21 @@
+<?php
+
+// Boolean
+$boolean = true;
+var_dump($boolean);
+
+// Entero
+$entero = 1;
+var_dump($entero);
+
+// Float
+$float = 20.5;
+var_dump($float);
+
+// String
+$string = "Manolito";
+var_dump($string);
+
+// Array
+$array = [];
+var_dump($array);
