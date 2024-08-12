402caf7abab4ea4051b0186ad1c83170f40c3ebf
Karim <kbiscaiburo@gmail.com>
2024-08-12 11:54:04 -0300

    Primeros pasos para ver en pantalla

---
+++ hola-mundo.php
@@ -0,0 +1,35 @@
+<?php 
+    
+?>
+<!-- Para imprimir codigo HTML es necesario cerrar o terminar con el codigo de php -->
+<h1>Hola Mundo</h1>
+
+<h2> <?php echo "Otro Hola Mundo"; ?> </h2>
+
+
+<?php echo "<h3>Otro Otro Hola Mundo</h3>"; 
+
+// Se puede escribir de esta manera tambien
+echo("Otra Manera"); 
+?>
+
+<br>
+<?php print("Con Print"); ?>
+
+<br>
+<?php print "Con Print sin parentesis"; ?>
+
+<br>
+<?php print_r("Si o si tiene que tener parentesis este"); ?>
+
+<br>
+<?php var_dump("Este ofrece un poco mas de informacion"); ?>
+
+<br>
+<?php var_dump(2); ?>
+
+
+
+
+
+
