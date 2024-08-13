1124aaaae06f86d09398cec0f9af13fa323708d0
Karim <kbiscaiburo@gmail.com>
2024-08-12 21:40:33 -0300

    Funciones con tipado

--- funciones.php
+++ funciones.php
@@ -8,3 +8,29 @@ function sumar (int $numero1 = 0, int $numero2 = 0) {
 
 sumar(10, "Hola");
 sumar(10, 5.5);
+
+
+// Indicamos que tiene que devolver un booleano
+function clienteAutenticado($cliente) : bool {
+    if ($cliente) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function autorizado(bool $estaAutorizado) : string | bool {
+    if ($estaAutorizado) {
+        return "Esta autorizado";
+    }
+
+    return "No esta autorizado";
+}
+
+function puedeDevolver(int $numero3) : ?int {
+    if ($numero3 > 50) {
+        return 1;
+    }
+
+    return null;
+}
